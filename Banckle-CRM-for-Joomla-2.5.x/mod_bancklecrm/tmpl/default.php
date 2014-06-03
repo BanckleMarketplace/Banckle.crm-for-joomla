c455a9a4a9c1ed743bc8290eada681aff6000c23
Imran Anwar <imranwar@gmail.com>
2014-06-03 16:12:42 +0500

    First Version

---
+++ Banckle-CRM-for-Joomla-2.5.x/mod_bancklecrm/tmpl/default.php
@@ -0,0 +1,17 @@
+<?php
+/*------------------------------------------------------------------------
+# mod_bancklecrm - Banckle CRM for Joomla
+# ------------------------------------------------------------------------
+# author    Masood Anwer <masood.anwer@banckle.com>
+# copyright Copyright (C) 2014 banckle.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://banckle.com
+-------------------------------------------------------------------------*/
+// no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<div id="banckleCRMWidget">
+<?php
+    echo $params->get("banckleCRMWidget");
+?>
+</div>
